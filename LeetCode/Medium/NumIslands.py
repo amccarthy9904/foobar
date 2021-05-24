b10fa68f8b074522f23cef9f5162e9609d0c9251
Aaron McCarthy <amccarthy9904@gmail.com>
2021-05-24 15:45:11 -0700

    adds header to numIslands

--- LeetCode/Medium/NumIslands.py
+++ LeetCode/Medium/NumIslands.py
@@ -1,3 +1,13 @@
+# 200. Number of Islands
+# Medium
+
+# Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), 
+# return the number of islands.
+# An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+# You may assume all four edges of the grid are all surrounded by water.
+
+# Runtime: 140 ms, faster than 65.74% of Python3 online submissions for Number of Islands.
+# Memory Usage: 22.7 MB, less than 5.11% of Python3 online submissions for Number of Islands.
 class Solution:
     def numIslands(self, grid: [[str]]) -> int:
         # m, n = len(grid), len(grid[0])
