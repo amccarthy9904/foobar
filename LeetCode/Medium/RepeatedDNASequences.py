b96f44240a92e2535fa1bf94346f063bfce995ed
arm <amccarthy9904@gmail.com>
2024-02-18 14:06:41 -0800

    done

--- LeetCode/Medium/RepeatedDNASequences.py
+++ LeetCode/Medium/RepeatedDNASequences.py
@@ -35,3 +35,16 @@ class Solution:
             else: d[nxt] = 0
                 
         return ret
+
+# better:
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+
+        seen, rep = set(), set()
+        for i in range(0, len(s) - 9):
+            sub = s[i:i+10]
+            if sub in seen:
+                rep.add(sub)
+            else:
+                seen.add(sub)
+        return list(rep)
